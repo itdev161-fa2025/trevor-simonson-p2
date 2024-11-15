417905fb2d285e32f0108ae7f3238a4aa1def464
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-11-15 11:55:47 -0600

    Note logic implemented

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -2,8 +2,41 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { NewNote } from './NewNote'
+import { useLocalStorage } from './useLocalStorage'
+import { useMemo } from 'react'
+
+export type Note = {
+    id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData ={
+    title: string
+    markdown: string
+    tagIds: string[]
+}
+export type NoteData ={
+    title: string
+    markdown: string
+    tags: Tag[]
+}
+
+export type Tag = {
+    id: string
+    label: string
+}
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(()=>{
+    return { ...notes, tags: tags.filter(tag => notes.tagIds.includes(tag.id))}
+  }, [notes, tags])
+
   return (
     <Container className='my-4'>
       <Routes>
--- note-app/src/NoteForm.tsx
+++ note-app/src/NoteForm.tsx
@@ -1,8 +1,28 @@
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable"
+import { NoteData, Tag } from "./App";
 
-export function NoteForm() {
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
+
+export function NoteForm({onSubmit}: NoteFormProps) {
+    const titleRef = useRef<HTMLInputElement>(null) 
+    const markdownRef = useRef<HTMLTextAreaElement>(null) 
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    
+    function handleSubmit(e: FormEvent){
+        e.preventDefault()
+
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: markdownRef.current!.value,
+            tags: []
+        })
+    }
+
     return(
         <Form>
             <Stack gap={4}>
@@ -10,19 +30,27 @@ export function NoteForm() {
                     <Col>
                         <Form.Group controlId="title">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control required />
+                            <Form.Control ref={titleRef} required />
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group controlId="tags">
                             <Form.Label>Tags</Form.Label>
-                            <CreatableReactSelect isMulti />
+                            <CreatableReactSelect value={ selectedTags.map(tag => {
+                                return { label: tag.label, value: tag.id}
+                            })} 
+                            onChange={tags=>{
+                                setSelectedTags(tags.map(tag=>{
+                                    return {label:tag.label, id: tag.value}
+                                }))
+                            }}
+                            isMulti />
                         </Form.Group>
                     </Col>
                 </Row>
                 <Form.Group controlId="markdown">
                     <Form.Label>Body</Form.Label>
-                    <Form.Control required as="textarea" rows={15} />
+                    <Form.Control required as="textarea" ref={markdownRef} rows={15} />
                 </Form.Group>
                 <Stack direction='horizontal' gap={2} className="justify-content-end">
                     <Button type="submit" variant="primary">Save</Button>
